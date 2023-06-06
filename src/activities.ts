abb05cfd3f7845560e00417d8a3f456e0890a36d
devrajash <ashdevrajttx@gmail.com>
2023-06-07 02:59:44 +0530

    new activity add

--- src/activities.ts
+++ src/activities.ts
@@ -2,4 +2,8 @@
 export async function greet(name: string): Promise<string> {
   return `Hello, ${name}!`;
 }
+
+export async function greetManipulate(name: string): Promise<string> {
+  return `${name}! How are You?`;
+}
 // @@@SNIPEND
